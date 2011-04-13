68d7ae7319832023a3b4a5b60519810775901468
Ariel Coppes <ariel.coppes@gemserk.com>
2011-04-13 17:54:28 -0300

    modified the arrow to get stuck when collision angle is less than 45 degrees, should check the other body to take further actions

--- archervsworld-core/src/main/java/com/gemserk/games/archervsworld/artemis/components/ArrowPhysicsBehavior.java
+++ archervsworld-core/src/main/java/com/gemserk/games/archervsworld/artemis/components/ArrowPhysicsBehavior.java
@@ -4,6 +4,7 @@ import com.artemis.Entity;
 import com.artemis.World;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.gemserk.commons.artemis.components.SpatialComponent;
 import com.gemserk.componentsengine.utils.AngleUtils;
@@ -17,8 +18,6 @@ public class ArrowPhysicsBehavior extends PhysicsBehavior {
 
 	private final ArcherVsWorldEntityFactory archerVsWorldEntityFactory;
 
-	private boolean shouldRemove = false;
-
 	private boolean shouldProcess = true;
 
 	public ArrowPhysicsBehavior(World world, Entity entity, ArcherVsWorldEntityFactory archerVsWorldEntityFactory) {
@@ -30,7 +29,7 @@ public class ArrowPhysicsBehavior extends PhysicsBehavior {
 	@Override
 	public void update(com.badlogic.gdx.physics.box2d.World world, Body body) {
 
-		if (!body.isAwake()) {
+		if (!body.isAwake() || body.getType() == BodyType.StaticBody) {
 			this.world.deleteEntity(entity);
 			SpatialComponent component = entity.getComponent(SpatialComponent.class);
 			archerVsWorldEntityFactory.createArrow(component.getPosition(), component.getAngle());
@@ -49,56 +48,23 @@ public class ArrowPhysicsBehavior extends PhysicsBehavior {
 
 	@Override
 	public void beginContact(Body body, Contact contact) {
-		shouldRemove = true;
 		shouldProcess = false;
-
-		// Fixture fixture = body.getFixtureList().get(0);
-		//
-		// Filter filterData = fixture.getFilterData();
-		//
-		// filterData.categoryBits = CollisionDefinitions.EnemiesGroup;
-		// filterData.maskBits = CollisionDefinitions.All & ~CollisionDefinitions.EnemiesGroup;
-		//
-		// fixture.setFilterData(filterData);
-
-		// if (!shouldProcess)
-		// return;
-		//
-		// shouldProcess = false;
-		//
-		// int numberOfContactPoints = contact.GetWorldManifold().getNumberOfContactPoints();
-		// if (numberOfContactPoints <= 0)
-		// return;
-		//
-		// System.out.println("contact points!");
-		//
-		// Vector2 contactPosition = contact.GetWorldManifold().getPoints()[0];
-		//
-		// Vector2 diff = contactPosition.cpy().sub(body.getTransform().getPosition());
-		// diff.nor();
-		//
-		// System.out.println(body.getTransform().getPosition());
-		// System.out.println(contactPosition);
-		//
-		// System.out.println(diff);
-		//
-		// float arrowAngle = (float) (body.getAngle() * 180 / Math.PI);
-		// // float angle = contactPosition.cpy().sub(body.getTransform().getPosition()).angle();
-		// float angle = diff.angle();
-		//
-		// float angle2 = contact.GetWorldManifold().getNormal().angle();
-		//
-		// System.out.println(angle2);
-		//
-		// if (angle2 < 25) {
-		// // shouldRemove = false;
-		// body.setType(BodyType.StaticBody);
-		// System.out.println("menor a 45");
-		// } else {
-		// System.out.println("mayor a 45");
-		// // body.setType(BodyType.StaticBody);
-		//
-		// }
+		
+		Vector2 normal = contact.GetWorldManifold().getNormal();
+		
+		float normalAngle = normal.cpy().mul(-1f).angle();
+		
+		float bodyAngle = (float) (body.getAngle() * 180.0 / Math.PI);
+		
+		double diff = Math.abs(angleUtils.minimumDifference(normalAngle, bodyAngle));
+		
+		int stickAngle = 45;
+		
+		if (diff < stickAngle)  {
+			body.setType(BodyType.StaticBody);
+		}
+		
+		// depend on the other body when defining whether to stuck or to do another thing...
 
 	}
 
